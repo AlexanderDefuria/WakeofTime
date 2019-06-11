78a9b9a4ce597a4d8780ed40a6baf2c466f94cd6
RobForg <RobertForgione12@gmail.com>
2019-06-11 18:28:09 -0400

    Created Project

---
+++ WakeofTimeEngine/cmake-build-debug/CMakeFiles/WakeofTimeEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WakeofTimeEngine.dir/main.cpp.o"
+  "WakeofTimeEngine.pdb"
+  "WakeofTimeEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WakeofTimeEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
